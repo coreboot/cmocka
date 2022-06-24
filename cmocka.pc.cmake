8ad2c4ed3d6d689ccf6ac1006fd9d26880eece81
Biswapriyo Nath <nathbappai@gmail.com>
2022-06-24 23:51:20 +0530

    cmake: Fix path relocation in pkgconfig file for mingw
    This fixes path relocation in mingw environment by using predefined
    Libs and Cflags variable in pkgconfig file. Otherwise, libdir and
    includedir are not shown in pkgconf output. e.g.
    * Without predefined variables:
      - pkgconf -cflags cmocka: No output
      - pkgconf -libs cmocka: -lcmocka
    * With predefined variables:
      - pkgconf -cflags cmocka: -IC:/msys64/mingw64/include
      - pkgconf -libs cmocka: -LC:/msys64/mingw64/lib -lcmocka
    
    Also official documentation suggests to use predefiend keywords here
    https://people.freedesktop.org/~dbn/pkg-config-guide.html
    
    Signed-off-by: Biswapriyo Nath <nathbappai@gmail.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- cmocka.pc.cmake
+++ cmocka.pc.cmake
@@ -1,5 +1,11 @@
-Name: ${PROJECT_NAME}
+# cmocka pkg-config source file
+
+prefix=@CMAKE_INSTALL_PREFIX@
+libdir=@CMAKE_INSTALL_FULL_LIBDIR@
+includedir=@CMAKE_INSTALL_FULL_INCLUDEDIR@
+
+Name: @PROJECT_NAME@
 Description: The cmocka unit testing library
-Version: ${PROJECT_VERSION}
-Libs: -L${CMAKE_INSTALL_FULL_LIBDIR} -lcmocka
-Cflags: -I${CMAKE_INSTALL_FULL_INCLUDEDIR}
+Version: @PROJECT_VERSION@
+Libs: -L${libdir} -lcmocka
+Cflags: -I${includedir}
