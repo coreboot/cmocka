a5cb5c33543aa5132a73cca4d1536627eb82b154
Eshan Kelkar <eshankelkar@galorithm.com>
2023-12-23 11:07:02 +0530

    Add unit tests for assert_false()
    Signed-off-by: Eshan Kelkar <eshankelkar@galorithm.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

---
+++ tests/test_assert_false_fail.c
@@ -0,0 +1,26 @@
+#include "config.h"
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <stdint.h>
+
+#include <cmocka.h>
+#include <cmocka_private.h>
+
+static void test_assert_false_fail(void **state)
+{
+    int x = 10;
+
+    (void)state; /* unused */
+    assert_false(x == 10);
+}
+
+int main(void)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_assert_false_fail),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
