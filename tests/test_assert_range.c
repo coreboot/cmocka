935ac8eb15c676853966e31e122718e4c9d00cbc
Norbert Pocs <norbertpocs0@gmail.com>
2025-04-03 12:29:10 +0200

    Add tests for assert_float(_not)_in_range
    Signed-off-by: Norbert Pocs <norbertpocs0@gmail.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- tests/test_assert_range.c
+++ tests/test_assert_range.c
@@ -42,12 +42,34 @@ static void test_assert_uint_not_in_range(void **state)
     assert_int_not_in_range(0, 1, UINTMAX_MAX);
 }
 
+static void test_assert_float_in_range(void **state)
+{
+    (void)state; /* unused */
+    assert_float_in_range(0.001, 0.001, 0.01, 0.00001);
+    assert_float_in_range(0.0001, 0.001, 0.1, 0.1);
+    assert_float_in_range(0.001, 0.001, 0.01, 0.001);
+    assert_float_in_range(0.3210, 0.20132013, 0.4013, 0.1);
+    assert_float_in_range(-4.32103, -5.5, 0.0, 1.0);
+}
+
+static void test_assert_float_not_in_range(void **state)
+{
+    (void)state; /* unused */
+    assert_float_not_in_range(0.0001, 0.01, 0.0001, 0.001);
+    assert_float_not_in_range(0.0001, 0.001, 0.1, 0.0001);
+    assert_float_not_in_range(0.1, 0.01, 0.001, 0.001);
+    assert_float_not_in_range(0.6210, 0.20132013, 0.4013, 0.1);
+    assert_float_not_in_range(-4.8, 0.2, -5.0, 0.1);
+}
+
 int main(void) {
     const struct CMUnitTest range_tests[] = {
         cmocka_unit_test(test_assert_int_in_range),
         cmocka_unit_test(test_assert_int_not_in_range),
         cmocka_unit_test(test_assert_uint_in_range),
         cmocka_unit_test(test_assert_uint_not_in_range),
+        cmocka_unit_test(test_assert_float_in_range),
+        cmocka_unit_test(test_assert_float_not_in_range),
     };
 
     return cmocka_run_group_tests(range_tests, NULL, NULL);
--- tests/test_assert_range_fail.c
+++ tests/test_assert_range_fail.c
@@ -44,6 +44,42 @@ static void test_assert_uint_not_in_range_fail_1(void **state)
     assert_uint_not_in_range(1, 0, UINTMAX_MAX);
 }
 
+static void test_assert_float_in_range_fail_1(void **state)
+{
+    (void)state; /* unused */
+    assert_float_in_range(0.001, 0.002, 2.0, 0.0001);
+}
+
+static void test_assert_float_in_range_fail_2(void **state)
+{
+    (void)state; /* unused */
+    assert_float_in_range(2.001, 0.002, 2.0, 0.0001);
+}
+
+static void test_assert_float_in_range_fail_3(void **state)
+{
+    (void)state; /* unused */
+    assert_float_in_range(-4.04, 0.0, -5.21, 0.1);
+}
+
+static void test_assert_float_not_in_range_fail_1(void **state)
+{
+    (void)state; /* unused */
+    assert_float_not_in_range(0.0001, 0.00001, 0.1, 0.001);
+}
+
+static void test_assert_float_not_in_range_fail_2(void **state)
+{
+    (void)state; /* unused */
+    assert_float_not_in_range(0.1, 0.00001, 0.1, 0.001);
+}
+
+static void test_assert_float_not_in_range_fail_3(void **state)
+{
+    (void)state; /* unused */
+    assert_float_not_in_range(0.1, 0.0, 0.2, 0.01);
+}
+
 int main(void) {
     const struct CMUnitTest range_fail_tests[] = {
         cmocka_unit_test(test_assert_int_in_range_fail_1),
@@ -52,6 +88,12 @@ int main(void) {
         cmocka_unit_test(test_assert_int_in_range_fail_4),
         cmocka_unit_test(test_assert_int_not_in_range_fail_1),
         cmocka_unit_test(test_assert_uint_not_in_range_fail_1),
+        cmocka_unit_test(test_assert_float_in_range_fail_1),
+        cmocka_unit_test(test_assert_float_in_range_fail_2),
+        cmocka_unit_test(test_assert_float_in_range_fail_3),
+        cmocka_unit_test(test_assert_float_not_in_range_fail_1),
+        cmocka_unit_test(test_assert_float_not_in_range_fail_2),
+        cmocka_unit_test(test_assert_float_not_in_range_fail_3),
     };
 
     return cmocka_run_group_tests(range_fail_tests, NULL, NULL);
