a50e6a5e590b256d1a61dfa6a2edb95639d4f916
Norbert Pocs <norbertpocs0@gmail.com>
2025-04-08 11:33:15 +0200

    Add tests for int_range
    Signed-off-by: Norbert Pocs <norbertpocs0@gmail.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- tests/test_expect_in_range.c
+++ tests/test_expect_in_range.c
@@ -16,6 +16,11 @@ static void mock_test_float(double value)
     check_expected_float(value);
 }
 
+static void mock_test_int(int value)
+{
+    check_expected(value);
+}
+
 static void test_expect_float_in_range(void **state)
 {
     (void)state; /* unused */
@@ -58,12 +63,54 @@ static void test_expect_float_not_in_range_count(void **state)
     mock_test_float(0.659);
 }
 
+static void test_expect_int_in_range(void **state)
+{
+    (void)state; /* unused */
+
+    expect_in_range(mock_test_int, value, 0, 10);
+    mock_test_int(0);
+}
+
+static void test_expect_int_in_range_count(void **state)
+{
+    (void)state; /* unused */
+
+    expect_in_range_count(mock_test_int, value, 0, 10, 4);
+    mock_test_int(0);
+    mock_test_int(5);
+    mock_test_int(1);
+    mock_test_int(10);
+}
+
+static void test_expect_int_not_in_range(void **state)
+{
+    (void)state; /* unused */
+
+    expect_not_in_range(mock_test_int, value, 0, 10);
+    mock_test_int(11);
+}
+
+static void test_expect_int_not_in_range_count(void **state)
+{
+    (void)state; /* unused */
+
+    expect_not_in_range_count(mock_test_int, value, 5, 10, 4);
+    mock_test_int(11);
+    mock_test_int(4);
+    mock_test_int(12);
+    mock_test_int(3);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_expect_float_in_range),
         cmocka_unit_test(test_expect_float_in_range_count),
         cmocka_unit_test(test_expect_float_not_in_range),
         cmocka_unit_test(test_expect_float_not_in_range_count),
+        cmocka_unit_test(test_expect_int_in_range),
+        cmocka_unit_test(test_expect_int_in_range_count),
+        cmocka_unit_test(test_expect_int_not_in_range),
+        cmocka_unit_test(test_expect_int_not_in_range_count),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
